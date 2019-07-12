fa5884654b78dc9523d8877172048d8c33ce4cb0
Max Williams <max.storyforge@gmail.com>
2019-07-11 21:42:17 -0700

    Added help text support for user-defined functions

--- Shell.cs
+++ Shell.cs
@@ -1,73 +1,101 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace CLISharp{
-    public class Shell
-    {
-        //protected delegate void funcdel(Shell s, params string[] args);
-        private bool toExit = false;
-        public string PromptPrefix{get;set;} = "> ";
-        public string WelcomeMessage{get;set;} = "Welcome. Please enter a command. Use \"exit\" or \"quit\" to close the program";
-        public string ExitMessage{get;set;} = "Goodbye";
-        public bool ShowWelcomeMessage{get;set;} = true;
-        public bool ShowExitMessage{get;set;} = true;
-        private Dictionary<string,ShellFunction> Functions = new Dictionary<string,ShellFunction>
-        {
-            {"test",new ShellFunction(Test)},
+namespace CLISharp
+{
+	public class Shell
+	{
+		//protected delegate void funcdel(Shell s, params string[] args);
+		private bool toExit = false;
+		public string PromptPrefix { get; set; } = "> ";
+		public string WelcomeMessage { get; set; } = "Welcome. Please enter a command. Use \"exit\" or \"quit\" to close the program";
+		public string ExitMessage { get; set; } = "Goodbye";
+		public bool ShowWelcomeMessage { get; set; } = true;
+		public bool ShowExitMessage { get; set; } = true;
+		private Dictionary<string, ShellFunction> Functions = new Dictionary<string, ShellFunction>
+				{
+            {"help",new ShellFunction(GetHelp)},
+            {"?", new ShellFunction(GetHelp)},
             {"exit",new ShellFunction(Exit)},
-            {"quit",new ShellFunction(Exit)}
-        };
-        public void Run()
-        {
-            string input = null;
-            if(ShowWelcomeMessage)
-            {
-                Console.WriteLine(WelcomeMessage);
-            }
-            do
-            {
-                Console.Write(PromptPrefix);
-                input = Console.ReadLine();
-                Execute(input);
-            }
-            while(!toExit);
-        }
-        private static void Test(Shell s, params string[] args)
-        {
-            Console.WriteLine($"Args for command {args[0]}: ");
-            for (var i = 1; i < args.Length;i++)
-            {
-                Console.Write($"{args[i]}, ");
-            }
-            Console.WriteLine();
-        }
-        private static void Exit(Shell s, params string[] args)
-        {
-            if(s.ShowExitMessage)
-            {
-                Console.WriteLine(s.ExitMessage);
-            }
-            s.toExit = true;
-        }
-        public void Execute(string input)
-        {
-            string[] ss = input.Split(' ');
-            input = input.ToLower();
-            if (Functions.ContainsKey(ss[0]))
-            {
-                Functions[ss[0]].Execute(this,ss);
-            }
-            else
-            {
-                Console.WriteLine($"Function '{input}' not recognized. Sorry!");
-            }
-        }
-        protected void RegisterFunctions(Dictionary<string,ShellFunction> func)
-        {
-            foreach (var function in func)
-            {
-                Functions.Add(function.Key.ToLower(),function.Value);
-            }
-        }
+						{"quit",new ShellFunction(Exit)}
+				};
+		public void Run()
+		{
+			string input = null;
+			if (ShowWelcomeMessage)
+			{
+				Console.WriteLine(WelcomeMessage);
+			}
+			do
+			{
+				Console.Write(PromptPrefix);
+				input = Console.ReadLine();
+				Execute(input);
+			}
+			while (!toExit);
+		}
+		private static void Exit(Shell s, params string[] args)
+		{
+			if (s.ShowExitMessage)
+			{
+				Console.WriteLine(s.ExitMessage);
+			}
+			s.toExit = true;
+		}
+		public void Execute(string input)
+		{
+			string[] ss = input.Split(' ');
+			input = input.ToLower();
+			if (Functions.ContainsKey(ss[0]))
+			{
+				Functions[ss[0]].Execute(this, ss);
+			}
+			else
+			{
+				Console.WriteLine($"Function '{input}' not recognized. Sorry!");
+			}
+		}
+		protected void RegisterFunctions(Dictionary<string, ShellFunction> func)
+		{
+			foreach (var function in func)
+			{
+				Functions.Add(function.Key.ToLower(), function.Value);
+			}
+		}
+    private static void GetHelp(Shell s, string[] args)
+    {
+      if (args.Length <= 1)
+      {
+        s.DisplayHelp();
+      }
+      else
+      {
+        s.DisplayHelp(args[1]);
+      }
     }
+		private void DisplayHelp(string funcKey)
+		{
+      string res = String.Empty;
+      if (Functions.ContainsKey(funcKey))
+      {
+        string text = Functions[funcKey].HelpText;
+        if(text != String.Empty && text != null)
+        {
+          res = $"{funcKey}: {text}";
+        }
+      }
+      else
+      {
+        res = $"Function '{funcKey}' not found.";
+      }
+      if (res != String.Empty) Console.WriteLine(res);
+		}
+		private void DisplayHelp()
+		{
+			foreach (var func in Functions)
+			{
+        DisplayHelp(func.Key);
+			}
+		}
+	}
 }
